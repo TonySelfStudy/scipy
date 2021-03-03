5ffaf50a7ead3812e9a427ef16994b28977ebf0f
Tony Held <tony.held@gmail.com>
2021-03-03 12:07:37 -0800

    Initial commit

---
+++ src/pandas/pandas_for_everyone_book/scratch.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+
+
+df2 = pd.DataFrame({'A': 1.,
+                    'B': pd.Timestamp('20130102'),
+                    'C': pd.Series(1, index=list(range(4)), dtype='float32'),
+                    'D': np.array([3] * 4, dtype='int32'),
+                    'E': pd.Categorical(["test", "train", "test", "train"]),
+                    'F': 'foo'})
+
+def func1():
+    local1 = 10
+    if True:
+        local2 = 15
+    print(local2)
+
+func1()
